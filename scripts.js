c05814d22dbd9c0598efcf5dd5cb3def9f973654
twodarkk <29480360+twodarkk@users.noreply.github.com>
2024-06-27 18:51:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,3 @@
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 const auth = firebase.auth();
 const db = firebase.database();
 
